5c78e0c1f1c20a479c0558689fae7b60588af1d0
Manoah VERDIER <verdier.developpement@gmail.com>
2020-09-01 10:40:38 +0200

    Numero service client

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -82,7 +82,7 @@ id="home-page"
             <div class="col-8 py-3 px-0 text-left valign-top">
                 <div class="first_line line mb-2">Service client</div>
                 <div class="second_line line">  
-                    <span class="w-100 d-inline-block">09 67 89 53 54</span>
+                    <span class="w-100 d-inline-block">07 86 10 13 46</span>
                     <a class="w-100 d-inline-block" href="mailto:e-commerce@prodice.net">e-commerce@prodice.net</a>
                 </div>
             </div>
@@ -111,7 +111,7 @@ id="home-page"
             <div class="col-3 py-3 px-0 text-left valign-top">
                 <div class="first_line line mb-2">Service client</div>
                 <div class="second_line line">  
-                    <span class="w-100 d-inline-block">09 67 89 53 54</span>
+                    <span class="w-100 d-inline-block">07 86 10 13 46</span>
                     <a class="w-100 d-inline-block" href="mailto:e-commerce@prodice.net">e-commerce@prodice.net</a>
                 </div>
             </div>
