325c52065eefc4454cbb92e3de3007ceabe86c9b
Manoah VERDIER <verdier.developpement@gmail.com>
2020-09-15 11:52:11 +0200

    images

--- resources/views/layouts/partials/product/image.blade.php
+++ resources/views/layouts/partials/product/image.blade.php
@@ -1,10 +1,14 @@
 <div class="product">
     @if ($discount ?? false)<span class="discount">-{{ $discount }}%</span>@endif
     <a href="{{ route('material', ['id' => $material->getKey(), 'name' => $material->label]) }}">
+        @if($material->image ?? false)
         <img 
             src="{{Config::get('filesystems.distant_img_roots.'.Config::get('filesystems.distant_img_root_default')).$image }}" 
             class="img-responsive"
         >
+        @else 
+        <img src="{{$image }}" class="img-responsive">
+        @endif
     </a>
 
     @if ($category_icon)
