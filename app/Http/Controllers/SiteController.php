fb5ae2d0e202e2a3e5699d0369805c9af85905ae
Manoah VERDIER <verdier.developpement@gmail.com>
2020-09-22 12:52:41 +0200

    fin debug

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -169,8 +169,8 @@ class SiteController extends Controller
                 ), function ($message) {
                     $message->from('sav@odice.cc');
                     $message
-                        ->to('verdier.developpement@gmail.com', 'SAV Odice')
-                        //->to('sav@odice.cc', 'SAV Odice')
+                        //->to('verdier.developpement@gmail.com', 'SAV Odice')
+                        ->to('sav@odice.cc', 'SAV Odice')
                         ->subject('Demande intervention');
                 }
             );
