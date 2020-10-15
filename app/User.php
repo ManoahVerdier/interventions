e51e4abb3f39872438950e1bdf37440707fe62f0
Manoah VERDIER <verdier.developpement@gmail.com>
2020-10-15 19:50:53 +0200

    test

--- app/User.php
+++ app/User.php
@@ -55,7 +55,7 @@ class User extends UccelloUser
         }
 
         $materials = Material::whereNotNull('domain_id')
-            ->where('domain_id', $domain_id);
+            ->where('site_id', session('site'));
         //dd($domain_id,session('site'));
         return $materials;
     }
