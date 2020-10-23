a08bd20d3aa01716922d6ff827f0912317bc63e0
Manoah VERDIER <verdier.developpement@gmail.com>
2020-10-23 16:16:36 +0200

    test

--- app/Site.php
+++ app/Site.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Site extends Model
 {
+    use SoftDeletes;
+    
     public function domain()
     {
         return $this->belongsTo(Domain::class);
